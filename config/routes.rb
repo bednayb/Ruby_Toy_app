132a23ab2f9d7ff472741c2994ceb7745c228396
bednayb <bednayb@gmail.com>
2017-05-12 11:52:36 +0200

    avaible to create- update-delete users

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # find at app/controllers/application_controller.rb
-  root 'application#hello'
+  resources :users
+  root 'users#index'
 end
